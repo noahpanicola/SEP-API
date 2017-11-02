1f0e7ced5d83069fa992df12e96bf346307bbf14
Tony Forsythe <forsythetony@gmail.com>
2017-11-02 17:36:49 -0500

    Deletes auth package

--- API/src/main/java/com/mydevgeek/controller/UserController.java
+++ API/src/main/java/com/mydevgeek/controller/UserController.java
@@ -1,17 +1,12 @@
 package com.mydevgeek.controller;
 
-import com.jwt.auth.JwtAuth;
 import com.mydevgeek.domain.User;
 import com.mydevgeek.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Date;
